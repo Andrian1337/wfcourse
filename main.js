7fbc303efa3ac76897a18494050c45efd31bd42f
Andrian1337 <andriyanichev@list.ru>
2024-05-02 23:18:23 +0400

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+const titleProject = "Оценка стоимости";
+const screensValue = `
+Шаблонные.
+С уникальным дизайном.
+С анимациями.
+`;
+let screenPrice = 1500;
+let percentage = 85;
+let fullPrice = 80000;
+let responsive = true;
 
-
-const firstName = "Nikita";
-let age = 22
-let sity = "Saratov"
+console.log (typeof screensValue)
