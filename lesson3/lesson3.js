8e8e318d01ca34442965d9b3e2cf7f05277aef82
Andrian1337 <122284880+Andrian1337@users.noreply.github.com>
2024-05-02 22:08:22 +0200

    Delete lesson3 directory

--- lesson3/lesson3.js
+++
@@ -1,8 +0,0 @@
-const titleProject = "Оценка стоимости";
-const screensValue = "Шаблонные, С уникальным дизайном, С анимациями";
-let screenPrice = 1500;
-let percentage = 85;
-let fullPrice = 80000;
-let responsive = true;
-
-    
